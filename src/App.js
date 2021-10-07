74fdaab1ac17d3fd62fa7ed8abf31f924a15e1a1
Abraham-maker <imnoteverywhere12@gmail.com>
2021-10-07 13:25:39 -0400

    error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [followers, setFollowers] = useState("");
+  const [following, setFollowing] = useState("");
+  const [repos, setRepos] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [bio, setBio] = useState("");
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/example`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  const setData = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url,
+    bio,
+    location,
+  }) => {
+    setName(name);
+    setUserName(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+    setLocation(location);
+    setBio(bio);
+  };
+
+  const handleSearch = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="github user" onChange={handleSearch} />
+        <button content="Search">ir</button>
+      </form>
+      <h1>{userName}</h1>
     </div>
   );
 }
